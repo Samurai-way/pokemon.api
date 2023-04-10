af187b3ea8626b807b53f546ac29a1e470ad0113
Samurai-way <olegmoishevych@gmail.com>
2023-04-11 01:16:29 +0300

    feat: cors errors

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,7 @@ const PORT = process.env.PORT || 3000;
 // export const corseSetup = (app: INestApplication) => {
 //   app.enableCors(getCorsOptions(origin));
 // };
+
 const origin = [
   'http://localhost:3001',
   'https://classy-naiad-2842bd.netlify.app',
@@ -28,9 +29,28 @@ async function start() {
       allowedHeaders: 'Content-Type,Authorization',
       methods: 'GET,PUT,POST,DELETE,OPTIONS',
       credentials: true,
+      preflightContinue: true,
     },
   });
   const app = createApp(rawApp);
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept',
+    );
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PUT, DELETE, OPTIONS',
+    );
+    // check if the request is a preflight request
+    if (req.method === 'OPTIONS') {
+      res.status(200).end();
+    } else {
+      next();
+    }
+  });
+
   await app.listen(PORT, () => {
     console.log(`[nest main] -> server started on http://localhost:${PORT}`);
   });
