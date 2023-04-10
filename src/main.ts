b777b94e058e9356c78d614e4c6f6ce6c56d3129
Samurai-way <olegmoishevych@gmail.com>
2023-04-10 22:51:24 +0300

    feat: cors errors

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,26 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as process from 'process';
 import { createApp } from './commons/createApp';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { INestApplication } from '@nestjs/common';
 
 const PORT = process.env.PORT || 3000;
 
+const origin = ['https://master--classy-naiad-2842bd.netlify.app'];
+
+const getCorsOptions = (origin: string[]): CorsOptions => ({
+  origin,
+  credentials: true,
+});
+
+export const corseSetup = (app: INestApplication) => {
+  app.enableCors(getCorsOptions(origin));
+};
+
 async function start() {
-  const rawApp = await NestFactory.create(AppModule, {
-    cors: { origin: ['https://master--classy-naiad-2842bd.netlify.app'] },
-  });
+  const rawApp = await NestFactory.create(AppModule);
   const app = createApp(rawApp);
+  corseSetup(app);
   await app.listen(PORT, () => {
     console.log(`[nest main] -> server started on http://localhost:${PORT}`);
   });
