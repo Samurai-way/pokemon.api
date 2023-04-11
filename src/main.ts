4ff02d6e8e0d2ed891755bdad96340c82dcf84ce
Samurai-way <olegmoishevych@gmail.com>
2023-04-11 11:57:14 +0300

    feat: cors errors

--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,41 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as process from 'process';
-import { createApp } from './commons/createApp';
 
 const PORT = process.env.PORT || 3000;
 
 async function start() {
-  const rawApp = await NestFactory.create(AppModule, {
-    cors: {
-      origin: '*',
-      allowedHeaders: 'Content-Type, Authorization',
-      methods: 'GET, PUT, POST, DELETE, OPTIONS',
-      credentials: true,
-      preflightContinue: true,
-      optionsSuccessStatus: 204,
-    },
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    allowedHeaders: 'Content-Type, Authorization',
+    methods: 'GET, PUT, POST, DELETE, OPTIONS',
+    credentials: true,
+    preflightContinue: true,
+    optionsSuccessStatus: 204,
   });
-
-  const app = createApp(rawApp);
-  app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-    );
-    res.setHeader(
-      'Access-Control-Allow-Methods',
-      'GET, POST, PUT, DELETE, OPTIONS',
-    );
-    // check if the request is a preflight request
-    if (req.method === 'OPTIONS') {
-      res.status(204).end();
-    } else {
-      next();
-    }
-  });
+  // const app = createApp(rawApp);
+  // app.use((req, res, next) => {
+  //   res.setHeader('Access-Control-Allow-Origin', '*');
+  //   res.setHeader(
+  //     'Access-Control-Allow-Headers',
+  //     'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  //   );
+  //   res.setHeader(
+  //     'Access-Control-Allow-Methods',
+  //     'GET, POST, PUT, DELETE, OPTIONS',
+  //   );
+  //   // check if the request is a preflight request
+  //   if (req.method === 'OPTIONS') {
+  //     res.status(204).end();
+  //   } else {
+  //     next();
+  //   }
+  // });
 
   await app.listen(PORT, () => {
     console.log(`[nest main] -> server started on http://localhost:${PORT}`);
   });
 }
+
 start();
